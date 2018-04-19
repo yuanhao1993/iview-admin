681f01cf89f476eb83f6cb8eeb183707c59b9aa8
Demon <750229099@qq.com>
2018-04-19 22:29:52 +0800

    完善渠道管理页面

--- src/views/channel/channel.vue
+++ src/views/channel/channel.vue
@@ -1,8 +1,28 @@
-<!--suppress ALL -->
+<style scoped lang="less">
+    .channel {
+        height: 100%;
+        overflow: auto;
+        .search {
+            padding: 0 0 20px;
+            /*display: flex;*/
+            /*flex-flow: row nowrap;*/
+            /*justify-content: flex-end;*/
+        }
+        .page {
+            padding: 20px 0;
+        }
+    }
+</style>
 <template>
-    <div class="channel">
-        <Table :loading="dataLoading" :columns="columns" :data="data"></Table>
-        <Page :total="100" @on-change="pageChange"></Page>
+    <div class="channel" ref="channel">
+        <div class="search" ref="search">
+            <Input v-model="searchText" placeholder="请输入搜搜..." style="width: 60%"/>
+            <span style="margin: 0 0 0 10px;"><Button type="primary" icon="search">搜索</Button></span>
+        </div>
+        <Table class="table" :height.sync="tableHeight" :loading="dataLoading" :columns="columns" :data="data"></Table>
+        <div class="page" ref="page">
+            <Page :total="100" @on-change="pageChange"></Page>
+        </div>
     </div>
 </template>
 
@@ -14,6 +34,7 @@
         data() {
             return {
                 dataLoading: true,
+                searchText: '',
                 data: [],
                 columns: [
                     {
@@ -35,40 +56,81 @@
                     {
                         title: '创建时间',
                         key: 'create_time'
+                    },
+                    {
+                        title: '操作',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.editData(params.index);
+                                        }
+                                    }
+                                }, '修改'),
+                                h('Button', {
+                                    props: {
+                                        type: 'error',
+                                        size: 'small'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.delData(params.index);
+                                        }
+                                    }
+                                }, '删除')
+                            ]);
+                        }
                     }
-                ]
+                ],
+                tableHeight: 'auto'
 
             };
         },
+        computed: {},
         created() {
             this.getlist(0);
         },
         methods: {
             getlist(index) {
                 this.dataLoading = true
-                fetchchannel({"limit": 10, "offset": 10 * index})
-                    .then(res => {
-                        let result = res.data.results.map(item => {
-                            let check_ways = item.check_ways_get.map(item => item.name);
-                            return {
-                                ...item,
-                                check_ways: check_ways.join(', ')
-                            }
-                        })
-                        this.data = result;
-                        this.dataLoading = false;
-                    }).catch();
+                fetchchannel({
+                    limit: 10,
+                    offset: 10 * index
+                }).then(res => {
+                    let result = res.data.results.map(item => {
+                        let checkWays = item.check_ways_get.map(item => item.name);
+                        return {
+                            ...item,
+                            check_ways: checkWays.join(', ')
+                        }
+                    })
+                    this.data = result;
+                    this.dataLoading = false;
+                }).catch();
             },
             pageChange(index) {
                 this.getlist(index - 1);
+            },
+            editData(index) {
+                alert(`修改${index}`);
+            },
+            delData(index) {
+                alert(`删除${index}`);
             }
+        },
+        mounted() {
+            this.tableHeight = this.$refs.channel.clientHeight - this.$refs.page.clientHeight - this.$refs.search.clientHeight;
         }
     };
 </script>
-
-<style scoped lang="less">
-    .channel{
-        height: 100%;
-
-    }
-</style>
